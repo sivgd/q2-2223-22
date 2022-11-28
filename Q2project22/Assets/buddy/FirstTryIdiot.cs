61f78b8281ac8285f00c161f329e059d823fc162
MUKILTEO\1709518 <1709518@mukilteo.wednet.edu>
2022-11-28 12:34:00 -0800

    first try

---
+++ Q2project22/Assets/buddy/FirstTryIdiot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTryIdiot : MonoBehaviour
+{
+
+    public GameObject Idiot;
+
+    void Start()
+    {
+        
+       
+
+
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
