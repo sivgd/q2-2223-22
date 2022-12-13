5d49ba20af907c7859534ce2d93369c88145513a
MUKILTEO\1709518 <1709518@mukilteo.wednet.edu>
2022-12-13 13:35:29 -0800

    title @ 60% compleate

--- Q2project22/Assets/buddy/TitleFunc.cs
+++ Q2project22/Assets/buddy/TitleFunc.cs
@@ -6,15 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class TitleFunc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void exit()
     {
-        
+        Debug.Log("If you arrre seeing this, the the application should quit. if the application has not quit, contact your local Unity Expert here: +45 33 92 00 00");
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Credits()
     {
-        
+        Debug.Log("cremdit");
+        SceneManager.LoadScene("credits");
+    }
+
+    public void ReadThis()
+    {
+        Debug.Log("controll");
+        SceneManager.LoadScene("controls");
+    }
+
+    public void Game()
+    {
+        Debug.Log("start!");
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void title()
+    {
+        Debug.Log("timtle");
+        SceneManager.LoadScene(0);
     }
 }
