a8c5f34f32ce07e3628a83dbbc48e1d4ac533de3
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-12-13 12:54:37 -0800

    player controller first person walk jump and look done

---
+++ Q2project22/Assets/andrea/scripts/playercam.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercam : MonoBehaviour
+{
+    public float senseX;
+    public float senseY;
+
+    public Transform Orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        Orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
