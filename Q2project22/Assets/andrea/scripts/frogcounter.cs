73b1e62892c9818e66f6f934c63c972a581f737c
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2023-01-09 11:55:32 -0800

    win actavted when u get 3 frogs in pond

--- Q2project22/Assets/andrea/scripts/frogcounter.cs
+++ Q2project22/Assets/andrea/scripts/frogcounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class frogcounter : MonoBehaviour
 {
@@ -29,6 +30,10 @@ public class frogcounter : MonoBehaviour
             Object.Destroy(thing.gameObject);
             frogCount++;
             frog3.gameObject.SetActive(true);
+        }
+        if(frogCount == 3)
+        {
+            SceneManager.LoadScene("Win");
         }
     }
     // Start is called before the first frame update
