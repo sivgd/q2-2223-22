618f2d7245a7d5009dca6ac528e599cef7a1c861
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2023-01-10 11:56:45 -0800

    Timer almost done

--- Q2project22/Assets/andrea/scripts/Timer.cs
+++ Q2project22/Assets/andrea/scripts/Timer.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float timevalue = 300;
+    public Text timetext;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +23,22 @@ public class Timer : MonoBehaviour
         }
         else
         {
-
+            SceneManager.LoadScene("lose");
         }
 
+        DisplayTime(timevalue);
+    }
 
+    void DisplayTime(float timetodisplay)
+    {
+        if(timetodisplay <0)
+        {
+            timetodisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timetodisplay / 60);
+        float seconds = Mathf.FloorToInt(timetodisplay % 60);
+
+        timetext.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
