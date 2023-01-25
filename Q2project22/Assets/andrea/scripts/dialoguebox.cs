6f9e71c8d02e3424655015738a19dc76d848046d
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2023-01-25 13:14:24 -0800

    dialogue fixed

--- Q2project22/Assets/andrea/scripts/dialoguebox.cs
+++ Q2project22/Assets/andrea/scripts/dialoguebox.cs
@@ -6,18 +6,19 @@ using UnityEngine.UI;
 
 public class dialoguebox : MonoBehaviour
 {
-    private Queue<string> sentences;
+    public Queue<string> sentences;
     public TextMeshProUGUI namet;
     public TextMeshProUGUI dialoguetextt;
     public Animator Animator;
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        //sentences = new Queue<string>();
     }
 
     public void StartDialogue(dialogue dialoguetext)
     {
+        sentences = new Queue<string>();
         Animator.SetBool("IsOpen", true);
         Debug.Log("Starting text" + dialoguetext.name);
         namet.text = dialoguetext.name;
